923d16ba60cb2eb1fea01c4431b99158c38fdbac
Luis Ansorge <lansorge@login8.hpc.binf.unibe.ch>
2025-09-24 14:43:42 +0200

    Quality control with FastQC script

---
+++ 01-run_fastqc.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#SBATCH --time=1-00:00:00
+#SBATCH --mem=16G
+#SBATCH --cpus-per-task=4
+#SBATCH --job-name=fastqc
+#SBATCH --partition=pibu_el8
+
+WORKDIR=/data
+OUTDIR=$WORKDIR/users/lansorge/assembly_annotation_course/read_QC
+
+apptainer exec --bind $WORKDIR \
+    /containers/apptainer/fastqc-0.12.1.sif \
+    fastqc -t 4 -o $OUTDIR $WORKDIR/users/lansorge/assembly_annotation_course/RNAseq_Sha/*.fastq.gz
